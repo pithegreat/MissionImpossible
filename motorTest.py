3d1bc75b5027fadb32013df44ab5f25399a0e970
pithegreat <pithegreat@users.noreply.github.com>
2018-01-02 21:53:02 -0500

    Add files via upload
    To test DC motors and calibrate turning times

---
+++ motorTest.py
@@ -0,0 +1,54 @@
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+
+import time
+import atexit
+
+mh = Adafruit_MotorHAT(addr=0x60) #creates the PVM to control the DC motors
+
+myLeftMotor = mh.getMotor(1) #creates the DC motor objects
+myRightMotor = mh.getMotor(2)
+
+myLeftMotor.setSpeed(255) #sets it to full speed
+myRightMotor.setSpeed(255)
+    
+def turnOffMotors(): #shuts down motors
+	myLeftMotor.run(Adafruit_MotorHAT.RELEASE)
+	myRightMotor.run(Adafruit_MotorHAT.RELEASE)
+
+def forward(seconds=None): #forward for number of seconds
+    myLeftMotor.run(Adafruit_MotorHAT.FORWARD)
+    myRightMotor.run(Adafruit_MotorHAT.FORWARD)
+    if seconds is not None:
+        time.sleep(seconds)
+        turnOffMotors()
+
+def backward(seconds=None): #backward for number of seconds
+    myLeftMotor.run(Adafruit_MotorHAT.BACKWARD)
+    myRightMotor.run(Adafruit_MotorHAT.BACKWARD)
+    if seconds is not None:
+        time.sleep(seconds)
+        turnOffMotors()
+
+def turnRight(seconds=None): #turn right for number of seconds
+    myLeftMotor.run(Adafruit_MotorHAT.FORWARD)
+    myRightMotor.run(Adafruit_MotorHAT.BACKWARD)
+    if seconds is not None:
+        time.sleep(seconds)
+        turnOffMotors()
+
+def turnLeft(seconds=None): #turn left for number of seconds
+    myLeftMotor.run(Adafruit_MotorHAT.BACKWARD)
+    myRightMotor.run(Adafruit_MotorHAT.FORWARD)
+    if seconds is not None:
+        time.sleep(seconds)
+        turnOffMotors()
+
+forward(5)
+backward(5)
+turnRight(5)
+turnLeft(5)
+
+
+
+ 
+atexit.register(turnOffMotors)
